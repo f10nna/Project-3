9c4cf0157e41780244f8d26bb663f8566a692c28
Fiona Greer <msfionagreer@gmail.com>
2023-05-15 18:07:05 -0700

    Created CuisineInfo component

---
+++ src/components/CuisineInfo.jsx
@@ -0,0 +1,9 @@
+function CuisineInfo({ cuisineType }) {
+  return (
+    <p className="cuisine-type">
+      <strong>{cuisineType}</strong>
+    </p>
+  )
+}
+
+export default CuisineInfo
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,9 +1,11 @@
+import CuisineInfo from './CuisineInfo'
+
 function RestaurantCard({ restaurantInfo }) {
   const { name, location, cuisine, image } = restaurantInfo
   return (
     <div className="restaurant-card">
       <h3>{name}</h3>
-      <p>{cuisine}</p>
+      <CuisineInfo cuisineType={cuisine} />
       <p>{location}</p>
       <img src={image} alt="Restaurant pic" />
     </div>
