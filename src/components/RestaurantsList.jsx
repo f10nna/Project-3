5a9115760f71152273e0ec179b9582d5b8d2c736
Fiona Greer <msfionagreer@gmail.com>
2023-05-15 18:14:41 -0700

    Removed h2 element from RestaurantList component

--- src/components/RestaurantsList.jsx
+++ src/components/RestaurantsList.jsx
@@ -2,7 +2,6 @@ import RestaurantCard from './RestaurantCard'
 function RestaurantList({ restaurants }) {
   return (
     <div className="restaurant-list-container">
-      <h2>Restaurant List</h2>
       {restaurants.map((restaurant) => {
         return <RestaurantCard restaurantInfo={restaurant} />
       })}
