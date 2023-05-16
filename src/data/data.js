c8887fdf7d9b238d8c2a8d46c59f8cfeab461714
Fiona Greer <msfionagreer@gmail.com>
2023-05-15 17:20:28 -0700

    Added mock data and created data directory.

---
+++ src/data/data.js
@@ -0,0 +1,100 @@
+export const restaurantData = [
+  {
+    id: 1,
+    name: 'Teleferic',
+    location: 'Palo Alto',
+    cuisine: 'Spanish',
+    image: 'https://example.com/teleferic.jpg'
+  },
+  {
+    id: 2,
+    name: 'Doppio Zero',
+    location: 'Mountain View',
+    cuisine: 'Italian',
+    image: 'https://example.com/doppio-zero.jpg'
+  },
+  {
+    id: 3,
+    name: 'Terún',
+    location: 'Palo Alto',
+    cuisine: 'Italian',
+    image: 'https://example.com/terun.jpg'
+  },
+  {
+    id: 4,
+    name: 'Auberge du soleil restaurant',
+    location: 'Napa',
+    cuisine: 'French',
+    image: 'https://example.com/auberge-du-soleil.jpg'
+  },
+  {
+    id: 5,
+    name: 'Nobu',
+    location: 'Palo Alto',
+    cuisine: 'Japanese',
+    image: 'https://example.com/nobu.jpg'
+  },
+  {
+    id: 6,
+    name: 'Oenotri',
+    location: 'Napa',
+    cuisine: 'Italian',
+    image: 'https://example.com/oenotri.jpg'
+  },
+  {
+    id: 7,
+    name: 'Rintaro',
+    location: 'San Francisco',
+    cuisine: 'Japanese',
+    image: 'https://example.com/rintaro.jpg'
+  },
+  {
+    id: 8,
+    name: 'Ding Tai Fung',
+    location: 'San Jose',
+    cuisine: 'Chinese',
+    image: 'https://example.com/ding-tai-fung.jpg'
+  },
+  {
+    id: 9,
+    name: 'Millennium',
+    location: 'Oakland',
+    cuisine: 'Vegan',
+    image: 'https://example.com/millennium.jpg'
+  },
+  {
+    id: 10,
+    name: 'Chez Panisse',
+    location: 'Berkeley',
+    cuisine: 'Californian',
+    image: 'https://example.com/chez-panisse.jpg'
+  },
+  {
+    id: 11,
+    name: 'La Bicyclette Restaurant',
+    location: 'Carmel',
+    cuisine: 'French',
+    image: 'https://example.com/la-bicyclette.jpg'
+  },
+  {
+    id: 12,
+    name: 'II Tegamino',
+    location: 'Carmel',
+    cuisine: 'Italian',
+    image: 'https://example.com/ii-tegamino.jpg'
+  },
+  {
+    id: 13,
+    name: 'Schooners Monterey',
+    location: 'Monterey',
+    cuisine: 'Seafood',
+    image: 'https://example.com/schooners-monterey.jpg'
+  },
+  {
+    id: 14,
+    name: 'Anatolian Kitchen',
+    location: 'Palo Alto',
+    cuisine: 'Mediterranean',
+    image: 'https://example.com'
+  }
+]
